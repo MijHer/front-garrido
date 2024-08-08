c45aad63e5283efa10d9fa71b15bff0a587e1a13
jdiegochg10 <jdiegochg10@protonmail.com>
2024-08-07 22:38:14 -0500

    remove logs

--- src/AppFooter.vue
+++ src/AppFooter.vue
@@ -1,8 +1,8 @@
 <template>
 	<div class="layout-footer">
-		<img alt="Logo" :src="footerImage()" height="20" class="mr-2" />
+		<!-- <img alt="Logo" :src="footerImage()" height="20" class="mr-2" />
 		by
-		<span class="font-medium ml-2">PrimeVue</span>
+		<span class="font-medium ml-2">PrimeVue</span> -->
 	</div>
 </template>
 
--- src/AppMenu.vue
+++ src/AppMenu.vue
@@ -1,9 +1,9 @@
 <template>
 	<div class="layout-menu-container">
 		<AppSubmenu :items="model" class="layout-menu" :root="true" @menuitem-click="onMenuItemClick" @keydown="onKeyDown" />
-		<a href="https://www.primefaces.org/primeblocks-vue" class="block mt-3">
+		<!-- <a href="https://www.primefaces.org/primeblocks-vue" class="block mt-3">
 			<img alt="primeblocks" :src="bannerImage()" class="w-full" />			
-		</a>
+		</a> -->
 	</div>
 </template>
 
